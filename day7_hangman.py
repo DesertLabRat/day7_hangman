4bddf289a5a1e0ffffe695829bb3ca395d5f209f
DesertBorn95 <delaney.seeburger@gmail.com>
2026-07-10 10:50:55 -0700

    improvements - input validation, track all guesses, update lives first

--- day7_hangman.py
+++ day7_hangman.py
@@ -3,45 +3,47 @@ from hangman_words import word_list
 import hangman_art
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 chosen_word = random.choice(word_list)
-print(f"Chosen word: {chosen_word}")
 print(hangman_art.logo)
 lives = 6
-correct_letters = []
+guessed_letters = set()
 game_over = False
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
+    print()
     display = ""
 
-    if guess not in alphabet:
-        print("***** NOT IN ALPHABET TRY AGAIN *****")
-        exit
+    if len(guess) != 1 or not guess.isalpha():
+        print("***** INVALID INPUT TRY AGAIN *****")
+        continue
 
-    if guess in correct_letters:
+    if guess in guessed_letters:
         print("***** LETTER ALREADY USED *****")
+        continue
     elif guess not in chosen_word:
+        lives -= 1
         if lives == 0:
             game_over = True
             print(f"***** YOU LOST! THE WORD WAS {chosen_word} *****")
         else:
             print(f"***** {guess} NOT IN THE WORD; LOSE A LIFE *****")
-            lives -= 1
 
     for letter in chosen_word:
         if letter == guess:
             display += letter
-            correct_letters.append(guess)
-        elif letter in correct_letters:
+            guessed_letters.add(guess)
+        elif letter in guessed_letters:
             display += letter
         else:
             display += "_"
 
-    print(hangman_art.stages[lives])
-    print(display)
-    print()
     if "_" not in display:
         game_over = True
-        print("***** YOU WON! *****")
+        print(f"***** YOU WON! THE WORD WAS {chosen_word} *****")
+    else:
+        print(hangman_art.stages[lives])
+        print(display)
+        print()
+
 
